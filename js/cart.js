1bfa99ee57850cacda83a0d6cf0302d8da77fbfd
20312 <jason.ziegler@vtsd.com>
2025-01-09 07:57:22 -0500

    Jquery progress

---
+++ js/cart.js
@@ -0,0 +1,10 @@
+
+    function addProduct(){
+      sessionStorage.setItem("price", "89.99")
+      UpdateTotal();
+    }
+   function updateTotal(){
+        $("inpt#total").val(sessionStorage.getItem("price"));
+    }
+
+});
